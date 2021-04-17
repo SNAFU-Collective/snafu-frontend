513b146ccc1c181f38e78e5b3bc4fd9f351efaf3
Dan <rusnac14@gmail.com>
2021-04-17 17:57:01 +0200

    Hide also first modal of wallet connect

--- src/App.vue
+++ src/App.vue
@@ -30,11 +30,11 @@ export default {
 <style>
 /* On screens that are 992px or less, set the background color to blue */
 @media screen and (min-width: 600px) {
-  .walletconnect-modal__base{
+  .walletconnect-modal__base .web3modal-modal-card{
     transform: translateY(0%) !important;
     top: 15px !important;
   }
-  .walletconnect-qrcode__base {
+  .walletconnect-qrcode__base .web3modal-modal-card{
     background: rgba(37, 41, 46, 0) !important;
   }
   .walletconnect-modal__header > p:nth-child(2) {
