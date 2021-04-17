c0dcab0d16dc333edbf846e20af70a62095945d0
Francesco Moca <francesco.moca@gmail.com>
2021-04-17 12:38:34 +0200

    Swap Card redesign

--- src/components/Swap/SwapCard.vue
+++ src/components/Swap/SwapCard.vue
@@ -8,18 +8,40 @@
         class="d-flex flex-column rounded-lg"
       >
         <v-spacer />
-        <v-row no-gutters justify="center"> <nft-input class="ma-3" /> </v-row>
-                <v-spacer />
+        <v-row no-gutters class="text-caption pa-3">
+          <v-col cols="11" class="text-caption"> FROM </v-col>
+          <v-col cols="1" align-self="start">
+            <v-tooltip right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                  color="grey"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                  class="text-h6"
+                >
+                  mdi-help-circle-outline
+                </v-icon>
+              </template>
+              <span>Tooltip</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="center">
+          <nft-input class="mx-3" />
+        </v-row>
 
         <v-row no-gutters justify="center">
           <v-btn large icon>
             <v-icon>mdi-swap-vertical</v-icon>
           </v-btn>
         </v-row>
-                <v-spacer />
 
+        <v-row no-gutters class="text-caption px-3">
+          <span> TO </span>
+        </v-row>
         <v-row no-gutters justify="center">
-          <snafu-input class="ma-3" />
+          <snafu-input class="mx-3" />
         </v-row>
         <v-spacer />
         <v-row no-gutters justify="center" align="end" class="rounded-0">
