d1de9adfb23f0aaff18d352114feeb81dc55093a
Francesco Moca <francesco.moca@gmail.com>
2021-04-18 15:09:14 +0200

    - Fix editions number - Update Tokens up to 57

--- src/store/modules/nftContract.js
+++ src/store/modules/nftContract.js
@@ -31,14 +31,14 @@ export default {
     actions: {
         async getNftsFromUser(context) {
             let userAddress = context.rootGetters["connectweb3/getUserAccount"];
-            context.dispatch("getNftsFromAddress", {address: userAddress, pool:false})
+            context.dispatch("getNftsFromAddress", { address: userAddress, pool: false })
         },
         async getNftsFromPool(context) {
-            context.dispatch("getNftsFromAddress", {address: snafu20Address, pool:true})
+            context.dispatch("getNftsFromAddress", { address: snafu20Address, pool: true })
         },
         async getNftsFromAddress(context, payload) {
             let erc1155 = context.rootGetters["connectweb3/getNftSnafu"];
-            let {address, pool} = payload;
+            let { address, pool } = payload;
             //console.log(erc1155)
             //let events = await erc1155.getPastEvents("TransferSingle", { fromBlock: minBlock, toBlock: 'latest' });
             //console.log(events)
@@ -50,16 +50,12 @@ export default {
                 fromBlock: minBlock,
                 toBlock: 'latest'
             });
-            console.log(events)
 
             let nfts = [];
 
             if (Array.isArray(events)) {
-
                 events = events.reverse();
-
                 for (let i = 0; i < events.length; i++) {
-
                     if (typeof events[i] == 'object') {
                         let nftIndex = nfts.findIndex(n => n.id === events[i].returnValues._id);
                         if (nftIndex === -1) {
@@ -80,13 +76,9 @@ export default {
             });
 
             if (Array.isArray(events)) {
-
                 events = events.reverse();
-
                 for (let i = 0; i < events.length; i++) {
-
                     if (typeof events[i] == 'object') {
-
                         for (let j = 0; j < events[i].returnValues[3].length; j++) {
                             let nftIndex = nfts.find(n => n.id === events[i].returnValues[3][j])
                             if (nftIndex === -1) {
@@ -99,17 +91,64 @@ export default {
                 }
             }
 
-            //TODO: sottrarre gli nft ritirati!
+            //Subtract withdrawn NFT
+            events = await erc1155.getPastEvents('TransferSingle', {
+                filter: {
+                    _from: address
+                },
+                fromBlock: minBlock,
+                toBlock: 'latest'
+            });
 
-            console.log(events);
-            console.log(nfts);
-            nfts.sort((a,b) => {
+
+            if (Array.isArray(events)) {
+                events = events.reverse();
+                for (let i = 0; i < events.length; i++) {
+                    if (typeof events[i] == 'object') {
+                        let nftIndex = nfts.findIndex(n => n.id === events[i].returnValues._id);
+                        if (nftIndex === -1) {
+                            //Should never happen
+                            nfts.push({ id: events[i].returnValues._id, editions: -events[i].returnValues._value });
+                        } else {
+                            nfts[nftIndex].editions -= events[i].returnValues._value;
+                        }
+                    }
+                }
+            }
+
+            events = await erc1155.getPastEvents('TransferBatch', {
+                filter: {
+                    _from: address
+                },
+                fromBlock: minBlock,
+                toBlock: 'latest'
+            });
+
+            if (Array.isArray(events)) {
+                events = events.reverse();
+                for (let i = 0; i < events.length; i++) {
+                    if (typeof events[i] == 'object') {
+                        for (let j = 0; j < events[i].returnValues[3].length; j++) {
+                            let nftIndex = nfts.find(n => n.id === events[i].returnValues[3][j])
+                            if (nftIndex === -1) {
+                                nfts.push({ id: events[i].returnValues[3][j], editions: -events[i].returnValues[4][j] });
+                            } else {
+                                nfts[nftIndex].editions -= events[i].returnValues[4][j];
+                            }
+                        }
+                    }
+                }
+            }
+
+            nfts = nfts.filter(n => n.editions > 0);
+
+            nfts.sort((a, b) => {
                 return +b.id - +a.id
             })
-            
-            if(pool){
+
+            if (pool) {
                 context.commit("setNftPool", nfts)
-            }else{
+            } else {
                 context.commit("setNftUser", nfts)
             }
 
