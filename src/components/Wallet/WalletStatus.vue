da9a695a24dfa676988850b1b7aa77562e98fd69
Dan <rusnac14@gmail.com>
2021-04-22 20:53:07 +0200

    Changed background to tooltips

--- src/components/Wallet/WalletStatus.vue
+++ src/components/Wallet/WalletStatus.vue
@@ -26,7 +26,7 @@
       </v-col>
 
       <v-col cols="1"  v-if="isMetamask">
-        <v-tooltip bottom>
+        <v-tooltip bottom color="rgb(0 0 0 / 89%)">
         <template v-slot:activator="{ on, attrs }">
           <v-btn
               color="grey"
@@ -47,7 +47,7 @@
       </v-tooltip>
       </v-col>
       <v-col cols="1"  v-if="isConnected">
-        <v-tooltip bottom>
+        <v-tooltip bottom color="rgb(0 0 0 / 89%)">
           <template v-slot:activator="{ on, attrs }">
             <v-btn
                 color="grey"
