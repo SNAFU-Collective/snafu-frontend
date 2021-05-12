f24bb1a8cede72262db43af220cb69aabe438b02
Dan <rusnac14@gmail.com>
2021-05-12 11:58:23 +0200

    Use 1hive rpc

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const snafuNftAddress = "0xED1eFC6EFCEAAB9F6d609feC89c9E675Bf1efB0a";
 export const snafu20Address = "0x27B9C2Bd4BaEa18ABdF49169054c1C1c12af9862";
 export const xdaiWebSocket = "wss://rpc.xdaichain.com/wss";
-export const xdaiRPC = "https://dai.poa.network";
+export const xdaiRPC = "https://xdai.1hive.org";
