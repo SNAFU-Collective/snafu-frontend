1415f00b621da2e2089e3fac9f52c279f2a829d1
Dan <rusnac14@gmail.com>
2021-05-16 02:49:38 +0200

    Created base modal

--- src/components/Wallet/WalletStatus.vue
+++ src/components/Wallet/WalletStatus.vue
@@ -26,25 +26,65 @@
       </v-col>
 
       <v-col cols="1"  v-if="isMetamask">
-        <v-tooltip bottom color="rgb(0 0 0 / 89%)">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-              color="grey"
-              dark
-              v-bind="attrs"
-              v-on="on"
-              class="text-h6"
-              icon
-              style="background-color: whitesmoke;margin-bottom: 10px"
-              small
-              @click="addSnafuToMetamask"
+        <v-dialog
+            v-model="dialog"
+            width="600px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                dark
+                v-bind="attrs"
+                v-on="on"
+                class="text-h6"
+                icon
+                style="background-color: whitesmoke;margin-bottom: 10px"
+                small
+                @click="askHelp"
+                color="grey"
+            >
+              ?
+            </v-btn>
+          </template>
+          <v-card>
+            <v-toolbar
+                light
+            >
+              <v-row>
+                <v-col cols="8" offset="2" >
+                  <v-card-title class="headline" style="justify-content: center; font-weight: lighter;" >
+                    Who are we
+                  </v-card-title>
+                </v-col>
+                <v-col cols="2" style="margin-top: 10px; text-align: right;">
+                  <v-btn
 
-          >
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </template>
-        <span>Add SNAFU to Metamask</span>
-      </v-tooltip>
+                      icon
+                      @click="dialog = false"
+                  >
+                    <v-icon>mdi-close</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-toolbar>
+            <v-card-text class="mt-15">
+              <v-row style="padding: 0px 10%; margin-bottom: 30px">
+                <v-col  cols="4" style="text-align: center">
+                  <a class="modalSocials">twitter</a>
+                </v-col>
+                <v-col  cols="4" style="text-align: center">
+                  <a class="modalSocials">telegram</a>
+                </v-col>
+                <v-col cols="4" style="text-align: center">
+                  <a class="modalSocials">github</a>
+                </v-col>
+              </v-row>
+              <v-row class="modalBody" >
+
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+<!--        <span>Add SNAFU to Metamask</span>-->
       </v-col>
       <v-col cols="1"  v-if="isConnected">
         <v-tooltip bottom color="rgb(0 0 0 / 89%)">
@@ -59,7 +99,6 @@
                 style="background-color: whitesmoke;margin-bottom: 10px;"
                 small
                 @click="disconnectWallet"
-
             >
               <v-icon size="22px">mdi-exit-to-app</v-icon>
             </v-btn>
@@ -78,8 +117,17 @@ import { mapFields } from 'vuex-map-fields';
 export default {
   // components: { SnafuBalance },
   components: {  },
+  data() {
+    return {
+      dialog: false,
+      showConfirmSwap: false,
+    };
+  },
     methods:{
-        ...mapActions("connectweb3", ["connectWallet", "addSnafuToMetamask", "disconnectWallet"])
+        ...mapActions("connectweb3", ["connectWallet", "addSnafuToMetamask", "disconnectWallet"]),
+      askHelp() {
+        this.showConfirmSwap = true;
+      }
     },
     computed:{
         ...mapFields("connectweb3", ["isConnected", "account", 'chainId']),
@@ -90,5 +138,18 @@ export default {
 </script>
 
 <style>
-
+.modalSocials {
+  font-weight: bold; font-size: 25px
+}
+a.modalSocials:hover {
+  color: #000000a3 !important;
+}
+.modalBody {
+  height: 300px;
+  margin: 30px 30px;
+  border: #00000045;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 5px;
+}
 </style>
