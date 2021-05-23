f09dadbdc696384e31421c38c38b7290c70ffbcc
Dan <rusnac14@gmail.com>
2021-05-23 19:53:34 +0200

    Show loading while fetching all nfts

--- src/components/Collection/NftsList.vue
+++ src/components/Collection/NftsList.vue
@@ -6,6 +6,13 @@
     <v-row class="mt-10">
       <nft-card v-for="nft in allNFTs" :key="nft.id" :nft="nft" class="ma-6" />
     </v-row>
+    <v-row v-if="allNFTs.length === 0" justify="center" class="pt-16">
+      <v-progress-circular
+          size="60"
+          indeterminate
+          color="black"
+      ></v-progress-circular>
+    </v-row>
   </div>
 </template>
 
