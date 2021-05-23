d567042b8517afb4a0de1428a3805528f5243efe
Dan <rusnac14@gmail.com>
2021-05-23 19:01:53 +0200

    Typo

--- src/components/Collection/NftsList.vue
+++ src/components/Collection/NftsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row justify="center" class="pt-15 mt-15">
-      <h2>ALL SNAFU NFTs:</h2>
+      <h2>ALL SNAFU NFTs</h2>
     </v-row>
     <v-row class="mt-10">
       <nft-card v-for="nft in allNFTs" :key="nft.id" :nft="nft" class="ma-6" />
