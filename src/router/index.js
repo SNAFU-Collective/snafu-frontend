aa83d152a7e8a0d7e510e7a825a8e4b91660df25
Dan <rusnac14@gmail.com>
2021-07-10 02:10:10 +0200

    Merged feature/price

--- src/router/index.js
+++ src/router/index.js
@@ -5,47 +5,47 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/NFTs.vue')
-  },
-  {
-    path: '/search',
-    name: 'Search',
-    // route level code-splitting
-    component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue')
-  },
-  {
-    path: '/wallet',
-    name: 'Wallet',
-    component: () => import('../views/Wallet.vue')
-  },
-  {
-    path: '/pool',
-    name: 'Pool',
-    component: Home
-  },
-    path: '/claim',
-    name: 'Claim',
-    component: () => import('../views/Claim.vue')
-  },
-  {
-  {
-    path: '/leaderboard',
-    name: 'Leaderboard',
-    component: () => import('../views/Leaderboard.vue')
-  },
-  {
-    path: '/user/:address',
-    name: 'User',
-    component: () => import('../views/User.vue'),
-    props: true
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/NFTs.vue'),
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        // route level code-splitting
+        component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue'),
+    },
+    {
+        path: '/wallet',
+        name: 'Wallet',
+        component: () => import('../views/Wallet.vue'),
+    },
+    {
+        path: '/pool',
+        name: 'Pool',
+        component: Home,
+    },
+    {
+        path: '/claim',
+        name: 'Claim',
+        component: () => import('../views/Claim.vue'),
+    },
+    {
+        path: '/leaderboard',
+        name: 'Leaderboard',
+        component: () => import('../views/Leaderboard.vue'),
+    },
+    {
+        path: '/user/:address',
+        name: 'User',
+        component: () => import('../views/User.vue'),
+        props: true,
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes,
 })
 
 export default router
