254bdbac007daf178a5b40e5e9bddfca94210dcf
Dan <rusnac14@gmail.com>
2021-08-03 13:05:17 +0200

    change rpc

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const snafu20Address = "0x27B9C2Bd4BaEa18ABdF49169054c1C1c12af9862";
 export const snafu721Address = "0x683B799b7ee741f00CCea34AcaDC32ba23E8982a";
 
 export const xdaiWebSocket = "wss://rpc.xdaichain.com/wss";
-export const xdaiRPC = "https://xdai.1hive.org";
+export const xdaiRPC = "https://rpc.xdaichain.com/";
 
 export const recaptchaSiteKey = "6Lfh9YgbAAAAACR-yXOXAqcGez4kETezd5iOU1ic";
 
