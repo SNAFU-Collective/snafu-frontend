c120350a52d285ebb1e2d172af2b3f7f0efc7205
Dan <rusnac14@gmail.com>
2021-11-24 11:49:47 +0100

    Randomize elements on home

--- src/components/Collection/NftsList.vue
+++ src/components/Collection/NftsList.vue
@@ -143,7 +143,7 @@ export default {
       let ids
       switch (this.currentTag) {
         case "all":
-          return this.allNFTs
+          return this.shuffle(this.allNFTs)
         case "collection3":
           ids = this.nfts.collection3
           break
@@ -194,6 +194,22 @@ export default {
         }
       }, 500)
     },
+    shuffle(arr) {
+      let len = arr.length;
+      let d = len;
+      let array = [];
+      let k, i;
+      for (i = 0; i < d; i++) {
+        k = Math.floor(Math.random() * len);
+        array.push(arr[k]);
+        arr.splice(k, 1);
+        len = arr.length;
+      }
+      for (i = 0; i < d; i++) {
+        arr[i] = array[i];
+      }
+      return arr;
+    }
   },
 }
 </script>
