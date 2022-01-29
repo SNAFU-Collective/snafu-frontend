82759de3c4e99cd047d0c09cb616e679c1cec9de
Dan <rusnac14@gmail.com>
2022-01-29 20:20:54 +0100

    Fixing responsivness

--- src/components/HomeComponents/TopBanner.vue
+++ src/components/HomeComponents/TopBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" style="text-align: left; display: flex" class="ma-10 topText">
-    <v-col cols-md="5" class="ma-6">
+    <v-col cols-md="5" colls-xs="12" class="ma-6 leftText">
       <h1><b>SNAFU Artist Collective</b></h1>
       <br>
       <h2 class="mt-5">Welcome to the home of the italian underground artists.</h2>
@@ -8,21 +8,13 @@
       <h2 class="mt-5">Here you can find exclusive <b>NFTs</b> and <b>Original Artworks</b>. Join the other collectors in <b>farming NFTs</b> or try to win one of them at the <b>lottery</b>!</h2>
     </v-col>
 
-    <v-col cols-md="5" style="justify-content: end">
+    <v-col v-if="allNFTs.length !== 0" cols-md="5" style="justify-content: end">
       <nft-card
-          v-if="allNFTs.length !== 0"
           :key="allNFTs.find(x => x.id === currentId+'').id"
           :nft="allNFTs.find(x => x.id === currentId+'')"
           :cardSize=400
           class="ma-6"
       />
-      <v-row v-else justify="center" class="pt-16">
-        <v-progress-circular
-            size="60"
-            indeterminate
-            color="black"
-        ></v-progress-circular>
-      </v-row>
     </v-col>
   </v-row>
 
@@ -73,23 +65,36 @@ export default {
 
 <style>
 
-.topText h1 {
-  color: #303030;
-  font-weight: bold;
-  font: 48px barlow-bold,sans-serif;
-}
 
-.topText h2 {
-  color: #303030;
-  font:normal normal normal 30px/1.4em barlow-medium,barlow,sans-serif;
-}
 
 @media screen and (min-width: 768px) {
+  .topText h1 {
+    color: #303030;
+    font-weight: bold;
+    font: 48px barlow-bold,sans-serif;
+  }
 
+  .topText h2 {
+    color: #303030;
+    font:normal normal normal 30px/1.4em barlow-medium,barlow,sans-serif;
+  }
 }
 
 @media screen and (max-width: 768px) {
+  .topText h1 {
+    color: #303030;
+    font-weight: bold;
+    font:30px barlow-bold,sans-serif;
+  }
 
+  .topText h2 {
+    color: #303030;
+    font:normal normal normal 15px/1.4em barlow-medium,barlow,sans-serif;
+  }
+
+  .leftText{
+    width: 100%;
+  }
 }
 
 </style>
