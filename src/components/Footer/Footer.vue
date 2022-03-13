5af3c0c48c9995db7a070ad030932eafbe2006bf
Dan <rusnac14@gmail.com>
2022-03-14 00:57:13 +0100

    justify text

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,88 +1,92 @@
 <template>
   <v-footer
-      dark
+      style="background-color: rgb(242, 242, 242)"
       padless
   >
-    <v-card
-        flat
-        tile
-        class="text-center"
-        style="background-color: #F2F2F2; width: 100%"
-    >
-      <v-card-text class="mt-5">
-        <v-btn
-            class="mx-4 black--text"
-            icon
-            @click="goTo('https://twitter.com/nftsnafu')"
-        >
-          <v-icon size="24px">
-            mdi-twitter
-          </v-icon>
-        </v-btn>
+    <v-container>
+      <v-card
+          flat
+          tile
+          class="text-center"
+          style="background-color: #F2F2F2; width: 100%"
+      >
+        <v-card-text class="mt-5">
+          <v-btn
+              class="mx-4 black--text"
+              icon
+              @click="goTo('https://twitter.com/nftsnafu')"
+          >
+            <v-icon size="24px">
+              mdi-twitter
+            </v-icon>
+          </v-btn>
 
-        <v-btn
-            class="mx-4 black--text"
-            icon
-            @click="goTo('https://discord.gg/kvbxMfUyQD')"
-        >
-          <v-icon size="24px">
-            mdi-discord
-          </v-icon>
-        </v-btn>
+          <v-btn
+              class="mx-4 black--text"
+              icon
+              @click="goTo('https://discord.gg/kvbxMfUyQD')"
+          >
+            <v-icon size="24px">
+              mdi-discord
+            </v-icon>
+          </v-btn>
 
-        <v-btn
-            class="mx-4 black--text"
-            icon
-            @click="goTo('https://t.me/joinchat/8BfTPOsJZ8o3YjJk')"
-        >
-          <v-icon size="24px">
-            mdi-send
-          </v-icon>
-        </v-btn>
+          <v-btn
+              class="mx-4 black--text"
+              icon
+              @click="goTo('https://t.me/joinchat/8BfTPOsJZ8o3YjJk')"
+          >
+            <v-icon size="24px">
+              mdi-send
+            </v-icon>
+          </v-btn>
 
-        <v-btn
-            class="mx-4 black--text"
-            icon
-            @click="goTo('https://github.com/SNAFU-Collective')"
-        >
-          <v-icon size="24px">
-            mdi-github
-          </v-icon>
-        </v-btn>
+          <v-btn
+              class="mx-4 black--text"
+              icon
+              @click="goTo('https://github.com/SNAFU-Collective')"
+          >
+            <v-icon size="24px">
+              mdi-github
+            </v-icon>
+          </v-btn>
 
-        <v-btn
-            class="mx-4 black--text"
-            icon
-            @click="goTo('https://www.info.nftsnafu.org/')"
-        >
-          <v-icon size="24px">
-            mdi-web
-          </v-icon>
-        </v-btn>
-      </v-card-text>
+          <v-btn
+              class="mx-4 black--text"
+              icon
+              @click="goTo('https://www.info.nftsnafu.org/')"
+          >
+            <v-icon size="24px">
+              mdi-web
+            </v-icon>
+          </v-btn>
+        </v-card-text>
 
-      <v-card-text class="black--text footerText" style="width: 100%">
-        <span>SNAFU is an Italian collective of artists composed of cyberpunks, painters, graphic designers, programmers, photographers, musicians, art lovers</span>
-        <br>
-        <span>who try to create a unique point of view regarding what can be conceived like Art in an era where everything is digital and unsubstantial.</span>
-        <br>
-        <span>At the same time, we are driven by the desire of creating a new form of sustenance for emerging artists exploiting new paradigms that blockchain and cryptocurrencies had brought.</span>
-        <br>  <br>
-        <span>We navigate in the digital ocean in a stubborn and contrary direction, because we are pirates and we want to take everything we deserve.</span>
-        <br>
-        <v-btn text class="font-weight-thin" style="color: black" @click="$router.push('/manifest')">
-          Read the manifest
-        </v-btn>
-      </v-card-text>
+        <v-card-text class="black--text footerText" style="width: 100%; text-align: justify">
+          <p>
+            SNAFU is an <b class="blueColor">Italian underground art collective</b> that established a platform
+            that leverages the new power NFTs have brought to the art world to <b class="blueColor">provide a steadier revenue stream to the global and local art community.</b>
+            The collective created a colorful and engaging platform (also called SNAFU) where artists can mint their work as NFTs and interact directly with patrons
+            and collectors.
+            <br>
+            We navigate in the digital ocean in a stubborn and contrary direction, because we are pirates and we want to take everything we deserve.
+          </p>
+        </v-card-text>
+        <v-row justify="center">
+          <v-btn text class="font-weight-thin" style="color: black" @click="$router.push('/manifest')">
+            Read the manifest
+          </v-btn>
+        </v-row>
 
-      <v-divider></v-divider>
+        <v-divider class="mt-10"></v-divider>
 
-      <v-card-text class="black--text">
-        <span>© 2022 by Collettivo SNAFU, Italia</span>
-        <br>
-        <a>hello@nftsnafu.org</a>
-      </v-card-text>
-    </v-card>
+        <v-card-text class="black--text">
+          <span>© 2022 by Collettivo SNAFU, Italia</span>
+          <br>
+          <a>hello@nftsnafu.org</a>
+        </v-card-text>
+      </v-card>
+    </v-container>
   </v-footer>
 </template>
 
