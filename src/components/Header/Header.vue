01481e7e523862c4389f4477cce4aa9f94c3ad7e
Dan <rusnac14@gmail.com>
2022-03-16 16:09:20 +0100

    REmove manifest from menu

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -14,7 +14,7 @@
         <router-link class="backHome" :to="{ name: 'Farm'}">FARM</router-link>
         <router-link class="backHome" :to="{ name: 'Community'}">COMMUNITY</router-link>
         <router-link class="backHome" :to="{ name: 'Catalog'}">CATALOG</router-link>
-        <router-link class="backHome" :to="{ name: 'Manifest'}">MANIFEST</router-link>
+<!--        <router-link class="backHome" :to="{ name: 'Manifest'}">MANIFEST</router-link>-->
       </v-col>
 
       <v-col cols="2" style="display: inline-flex;align-items: center;text-decoration: none !important; color: #303030">
@@ -41,7 +41,7 @@
           <router-link class="backHome" :to="{ name: 'Farm'}">FARM</router-link>
           <router-link class="backHome" :to="{ name: 'Community'}">COMMUNITY</router-link>
           <router-link class="backHome" :to="{ name: 'Catalog'}">CATALOG</router-link>
-          <router-link class="backHome" :to="{ name: 'Manifest'}">MANIFEST</router-link>
+<!--          <router-link class="backHome" :to="{ name: 'Manifest'}">MANIFEST</router-link>-->
 <!--          <a class="backHome" @click="openBuySnafuModal()">BUY SNAFU <v-icon class="backHome arrowRedirect"> mdi-arrow-top-right </v-icon></a>-->
 <!--          <a class="backHome" @click="openVoteModal()">VOTE <v-icon class="backHome arrowRedirect"> mdi-arrow-top-right </v-icon></a>-->
 <!--&lt;!&ndash;          <a class="backHome" href="https://www.nftsnafu.org/farms" target="_blank">FARM <v-icon class="backHome arrowRedirect"> mdi-arrow-top-right </v-icon></a>&ndash;&gt;-->
