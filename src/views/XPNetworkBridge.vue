0ea4a6709955a0c3e1fc450f63fb05f2409bbe22
Dan Rusnac <rusnac14@gmail.com>
2022-03-24 13:50:15 +0100

    new iframe

--- src/views/XPNetworkBridge.vue
+++ src/views/XPNetworkBridge.vue
@@ -1,5 +1,7 @@
 <template>
-  <iframe style="height: 1000px" src='https://widget.xp.network/?widget=true&background=ffffff&color=000000&fontSize=16&btnColor=ffffff&btnBackground=000000&btnRadius=9&fontFamily=Roboto&chains=BSC-xDai&cardBackground=ffffff&cardRadius=25&secondaryColor=707070&accentColor=000000&borderColor=000000&iconColor=000000&wallets=MetaMask-WalletConnect&bridgeState=undefined&showLink=true' frameborder='0' width="100%" height="100%"></iframe>
+  <div style="height: 1000px; text-align: center">
+    <iframe src="https://widget-staging.xp.network/?widget=true&amp;background=ffffff&amp;color=0d0d0d&amp;fontSize=16&amp;btnColor=ffffff&amp;btnBackground=000000&amp;btnRadius=9&amp;fontFamily=Roboto&amp;chains=BSC-xDai&amp;cardBackground=1e222d&amp;cardRadius=25&amp;secondaryColor=242424&amp;accentColor=10193d&amp;borderColor=37405b&amp;iconColor=000000&amp;wallets=MetaMask-WalletConnect&amp;bridgeState=undefined&amp;showLink=true&amp;cardColor=ffffff&amp;cardBackgroundBot=3e4454" frameborder="0" width="80%" height="100%"></iframe>
+  </div>
 </template>
 <script>
 
