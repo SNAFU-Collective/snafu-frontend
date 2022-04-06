8dd8e2591b4851a27c8a9b7b40f075aadc0af5bb
Dan <rusnac14@gmail.com>
2022-04-06 09:48:54 +0200

    https://rpc.nftsnafu.org/

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const snafuNftAddress = "0xED1eFC6EFCEAAB9F6d609feC89c9E675Bf1efB0a";
 export const snafu20Address = "0x27B9C2Bd4BaEa18ABdF49169054c1C1c12af9862";
 export const snafu721Address = "0x683B799b7ee741f00CCea34AcaDC32ba23E8982a";
 
-export const xdaiRPC = "https://gnosis-rpc.genuino.world/";
+export const xdaiRPC = "https://rpc.nftsnafu.org/";
 
 export const recaptchaSiteKey = "6Lfh9YgbAAAAACR-yXOXAqcGez4kETezd5iOU1ic";
 
